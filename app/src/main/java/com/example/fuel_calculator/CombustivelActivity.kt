764a418bbfd23619b9db16624fbdae693fa24b7a
Heberth Scofield <139893739+Scofield170@users.noreply.github.com>
2024-06-28 19:43:32 +0200

    Telas + atualizacao nas Activitys
    add tela 3
    add fucao para os botoes
    
    snackbar
    
    Acitivitys atualizadas

--- app/src/main/java/com/example/fuel_calculator/CombustivelActivity.kt
+++ app/src/main/java/com/example/fuel_calculator/CombustivelActivity.kt
@@ -1,15 +1,44 @@
 package com.example.fuel_calculator
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
 
 class CombustivelActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_combustivel)
         enableEdgeToEdge()
+
+        val button2 = findViewById<Button>(R.id.button2)
+        val edtCombustivel = findViewById<EditText>(R.id.preco_combustivel)
+
+        button2.setOnClickListener {
+            val combustivelStr: String = edtCombustivel.text.toString()
+
+            if (combustivelStr.isEmpty()) {
+
+                Snackbar
+                    .make(
+                        edtCombustivel,
+                        "Preencha todos os campos",
+                        Snackbar.LENGTH_LONG
+                    )
+                    .show()
+
+            } else {
+                val combustivel = combustivelStr.toFloat()
+
+                val intent = Intent(this, ConsumoActivity::class.java)
+                intent.putExtra(KEY_COMBUSTIVEL, combustivel)
+                startActivity(intent)
+            }
         }
     }
+}
---
+++ app/src/main/java/com/example/fuel_calculator/ConsumoActivity.kt
@@ -0,0 +1,20 @@
+package com.example.fuel_calculator
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+const val KEY_COMBUSTIVEL = "ConsumoActivity.KEY_COMBUSTIVEL"
+
+class ConsumoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_consumo)
+
+        val valorCombustivel = intent.getFloatExtra(KEY_COMBUSTIVEL, 0f)
+
+    }
+}
