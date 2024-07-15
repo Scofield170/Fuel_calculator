f74ca1db6e3028880a6729ae98c97a1561628643
Heberth Scofield <139893739+Scofield170@users.noreply.github.com>
2024-07-15 18:53:02 +0200

    app final
    - atualizado os layouts
    - formatação dos tipos
    - correção na activity resultado
    - add símbolos
    - correção nas apresentações dos textView

--- app/src/main/java/com/example/fuel_calculator/ResultadoActivity.kt
+++ app/src/main/java/com/example/fuel_calculator/ResultadoActivity.kt
@@ -20,23 +20,36 @@ class ResultadoActivity : AppCompatActivity() {
 
         val resultado = intent.getFloatExtra(KEY_RESULTADO, 0f)
         val tvResultado = findViewById<TextView>(R.id.textView_resultado)
+        val formatacaoResultado = String.format("%.2f", resultado)
 
-        tvResultado.text = resultado.toString()
+        tvResultado.text = formatacaoResultado + "€"
 
         val preco = intent.getFloatExtra(KEY_PRECO, 0f)
         val tvPreco = findViewById<TextView>(R.id.textView_preco)
 
-        tvPreco.text = preco.toString()
+        tvPreco.text = preco.toString() + "€"
 
         val distancia = intent.getFloatExtra(KEY_DISTANCIA2, 0f)
         val tvDistancia = findViewById<TextView>(R.id.textView_distancia)
 
-        tvDistancia.text = distancia.toString()
+        val formatacaoDistancia = if (distancia == distancia.toInt().toFloat()) {
+            String.format("%d", distancia.toInt())
+        } else {
+            String.format("%.3f", distancia)
+        }
+
+        tvDistancia.text = formatacaoDistancia + " Km"
 
         val consumo = intent.getFloatExtra(KEY_CONSUMO1, 0f)
         val tvConsumo = findViewById<TextView>(R.id.textView_consumo)
 
-        tvConsumo.text = consumo.toString()
+        val formatacaoConsumo = if (consumo == consumo.toInt().toFloat()) {
+            String.format("%d", consumo.toInt())
+        } else {
+            String.format("%.2f", consumo)
+        }
+
+        tvConsumo.text = formatacaoConsumo + " Km/l"
 
         val buttonRecomecar = findViewById<Button>(R.id.button_recomecar)
 
